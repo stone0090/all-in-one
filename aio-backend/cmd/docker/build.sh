2a3f6d237f52e0179d4760176f96269a4a10bc19
stone0090 <stone0090@126.com>
2023-06-05 23:22:54 +0800

    feat: 优化配置，调通docker部署。

--- aio-backend/cmd/docker/build.sh
+++ aio-backend/cmd/docker/build.sh
@@ -1,4 +1,6 @@
+path=$(pwd)
 version=$(date "+%Y%m%d%H%M%S")
+cd ../../
 mvn -e -B -U clean package -Dmaven.test.skip=true
-docker build -t aio:${version} .
+docker build -t aio:${version} -f "$path/Dockerfile" .
 docker run -dp 9090:9090 aio:${version}
